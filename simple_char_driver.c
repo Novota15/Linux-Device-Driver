4e04aa78b84e8f2824bb0a9c564d917729687ec6
Grant Novota <47872713+Novota15@users.noreply.github.com>
2019-06-18 09:03:36 -0600

    Update simple_char_driver.c

--- simple_char_driver.c
+++ simple_char_driver.c
@@ -9,6 +9,9 @@
 #define BUFFER_SIZE 1024
 
 /* Define device_buffer and other global data structures you will need here */
+#define DEVICE_NAME "simplechardev"
+static int Major;
+static int Device_Open = 0;
 
 
 ssize_t simple_char_driver_read (struct file *pfile, char __user *buffer, size_t length, loff_t *offset)
@@ -56,19 +59,32 @@ struct file_operations simple_char_driver_file_operations = {
 
 	.owner   = THIS_MODULE,
 	/* add the function pointers to point to the corresponding file operations. look at the file fs.h in the linux souce code*/
+	.read = simple_char_driver_read,
+    .write = simple_char_driver_write,
+    .open = simple_char_driver_open,
+    .close = simple_char_driver_close,
+    .llseek = simple_char_driver_seek
 };
 
 static int simple_char_driver_init(void)
 {
 	/* print to the log file that the init function is called.*/
+	printk(KERN_ALERT "%s function called\n",__FUNCTION__);
 	/* register the device */
+	Major = register_chrdev(0, NAME, &simple_char_driver_file_operations);
+	if (Major < 0) {
+		printk(KERN_ALERT "registering failed")
+	}
 	return 0;
 }
 
 static void simple_char_driver_exit(void)
 {
 	/* print to the log file that the exit function is called.*/
+	printk(KERN_ALERT "%s function called\n",__FUNCTION__);
 	/* unregister  the device using the register_chrdev() function. */
 }
 
 /* add module_init and module_exit to point to the corresponding init and exit function*/
+module_init(simple_char_driver_init);
+module_exit(simple_char_driver_exit);
